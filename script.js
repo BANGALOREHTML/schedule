c6c525d1d9c63baf83bf9a4f6e321acc66f5aa2c
Dez <dezd24@gmail.com>
2022-10-25 19:37:58 -0700

    css and starting local storage

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+localStorage.setItem('schedule', JSON.stringify(schedule));
+// Path: schedule/script.js
+var schedule = JSON.parse(localStorage.getItem('schedule'));
+// Path: schedule/script.js
+localStorage.removeItem('schedule');
+// Path: schedule/script.js
+localStorage.clear();
+// Path: schedule/script.js
+localStorage.key(0);
+// Path: schedule/script.js
+localStorage.length;
+// Path: schedule/script.js
 
