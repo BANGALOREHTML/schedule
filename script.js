191bd9492d60c388d8124d2a00fd78bac3540482
Dez <dezd24@gmail.com>
2022-10-27 19:33:55 -0700

    more css and live time

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let description = $("#description");
+let currentDay = $("#currentDay");
 
 function currentTime() {
     let day = moment().format("MMM Do YYYY, HH:mm:ss");
     currentDay.text(day);
+    // console.log(day)
 }
 
 setInterval(currentTime, 1000);
 function updatePPFColor() {
     let currentHour = moment().format("H");
-    for (let i = 09; i <= 17; i++) {
+    for (let i = 09; i <= 12; i++) {
         if (i < currentHour) {
             $("#" + i).addClass("past");
         } else if (i == currentHour) {
@@ -19,7 +21,7 @@ function updatePPFColor() {
     }
 }
 
-updatePPFColor();
+// updatePPFColor();
 
 $(".saveBtn").click(function () {
     let timeIndex = $(this).parent().attr("id");
@@ -29,12 +31,12 @@ $(".saveBtn").click(function () {
     console.log(timeIndex, daySchedule);
 });
 function getStoredSchedule() {
-    for (let i = 9; i <= 17; i++) {
+    for (let i = 9; i <= 12; i++) {
         let storedSchedule = localStorage.getItem(i);
         $("#" + i)
             .find("textarea")
             .text(storedSchedule);
     }
 }
-
+currentTime();
 getStoredSchedule();
